092117f263cd9a86a98080f3b326fa6fd2b0d55b
Michael Caines <michael.caines@nscc.ca>
2026-07-22 16:35:32 +0000

    fix: update link for Classroom 50 guides to point to GitHub Classroom

--- docs-site/docusaurus.config.js
+++ docs-site/docusaurus.config.js
@@ -199,7 +199,7 @@ const config = {
             items: [
               {
                 label: 'Classroom 50',
-                to: `${latestVersionPath}/guides/classroom50`,
+                to: `${latestVersionPath}/guides/github-classroom`,
               },
               {
                 label: 'Example Workflows',
